0b2d4cd7ec5e8f1b631394eb697b4a4995fe9346
Jihun Lee <wlgns3398@naver.com>
2022-01-08 00:04:15 +0900

    05_큐

--- 05_07_교육과정설계(큐).py
+++ 05_07_교육과정설계(큐).py
@@ -1,27 +1,7 @@
 import sys
 from collections import deque
-sys.stdin = open("input.txt","rt")
+#sys.stdin = open("input.txt","rt")
 
-need = input()
-
-n = int(input())
-
-for i in range(n):
-    plan = input()
-    dq = deque(need)
-    for x in plan:
-        if x in dq:
-            if x!= dq.popleft():
-                print("#%d NO"%(i+1))
-                break
-    else:
-        if len(dq) ==0:
-            print("#%d YES"%(i+1))
-        else:
-            print("#%d NO"%(i+1))
-    
-#내풀이
-"""
 a = input()
 
 n = int(input())
@@ -51,4 +31,4 @@ for i in range(n):
         print("#{} YES".format(i+1))
     else:
         print("#{} NO".format(i+1))
-"""
+    
