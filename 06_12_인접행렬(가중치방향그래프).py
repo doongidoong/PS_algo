c1c56a44fe8f719349928265cfca90db50dc11ca
Jihun Lee <wlgns3398@naver.com>
2022-01-14 16:14:43 +0900

    06_DFS(그래프)

---
+++ 06_12_인접행렬(가중치방향그래프).py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt","rt")
+
+n, m = map(int ,input().split()) #n은 노드의 개수 m은 간선의 개수
+
+g = [[0]*(n+1) for _ in range(n+1)]
+
+
+for i in range(m):
+    a,b,c=  map(int ,input().split())
+    g[a][b] =c
+
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(g[i][j],end=' ')
+    print() 
---
+++ 06_13_경로탐색.py
@@ -0,0 +1,33 @@
+import sys
+#sys.stdin = open("input.txt","rt")
+def dfs(v):
+    global cnt 
+    if v==n:
+        cnt+=1
+        #for x in path: 
+        #    print(x,end = ' ')  경로 출력
+        return
+    else:
+        for i in range(1,n+1):
+            if ch[i] ==0 and g[v][i]==1:
+                ch[i] =1 
+                path.append(i)
+                dfs(i)
+                path.pop()
+                ch[i] = 0 
+
+
+n , m = map(int,input().split())
+g = [[0]*(n+1) for _ in range(n+1)]
+ch = [0]*(n+1)
+for i in range(m):
+    a, b = map(int,input().split())
+    g[a][b] = 1
+
+
+cnt =0
+path = []  # 해당 문제에서는 필요없지만 경로를 알아보기위해 넣음
+path.append(1)
+ch[1]=1
+dfs(1)
+print(cnt)
