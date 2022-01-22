ea5c7248cc7a781f6bd17cd8e3326f00e0bd66c3
Jihun Lee <wlgns3398@naver.com>
2022-01-22 22:37:26 +0900

    08_DP

--- algorithm/08_07_가장높은탑쌓기(DP).py
+++ algorithm/08_07_가장높은탑쌓기(DP).py
@@ -1,6 +1,6 @@
 import sys
 
-#sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\algorithm\\input.txt","r")
+sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\algorithm\\input.txt","r")
 
 n= int(input())
 a = [ list(map(int,input().split())) for _ in range(n)]
@@ -15,7 +15,7 @@ for i in range(1,n):
     k=0
     maxh=0
     while(k<i):
-        if a[k][0] > a[i][0] and a[k][2] > a[i][2] and maxh<dy[k]:
+        if  a[k][2] > a[i][2] and maxh<dy[k]:
             maxh=dy[k]
         k+=1
     dy[i] = maxh + a[i][1]
