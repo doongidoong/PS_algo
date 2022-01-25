aa7db332103be0f985db647601d9d3f92d77a0b3
Jihun Lee <wlgns3398@naver.com>
2022-01-25 17:25:54 +0900

    큐

---
+++ baekjoon/1158.py
@@ -0,0 +1,27 @@
+import imp
+import sys
+from collections import deque 
+sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+
+n, k = map(int, input().split())
+Q =deque()
+for i in range(1,n+1):
+    Q.append(i)
+res =[]
+cur =1
+while Q:
+    tmp =Q.popleft()
+    if cur==k:
+        res.append(tmp)
+        cur=1
+    else:
+        Q.append(tmp)
+        cur+=1
+print('<', end='')
+for i in range(len(res)):
+    if i == len(res)-1:
+        print(res[i],end='')
+        break
+    print(res[i], end=', ')
+
+print('>')
