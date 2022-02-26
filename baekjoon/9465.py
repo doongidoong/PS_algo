6c3359822796e4d824e1211edfab1201ec1026cc
Jihun Lee <wlgns3398@naver.com>
2022-02-26 20:20:13 +0900

    DP

--- baekjoon/9465.py
+++ baekjoon/9465.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+#sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
 k = int(input())
 for i in range(k):
     n = int(input())
@@ -10,9 +10,9 @@ for i in range(k):
             dy[0][0] = L[0][0]
             dy[1][0] = L[1][0]
         elif i==1:
-            dy[0][1] = L[1][0] +L[0][1]
+            dy[0][1] = L[1][0] +L[0][1] 
             dy[1][1] = L[0][0] +L[1][1]
         else: 
             dy[0][i] = max(dy[1][i-2],dy[1][i-1],dy[0][i-2])+L[0][i]
             dy[1][i] = max(dy[0][i-2],dy[0][i-1],dy[1][i-2])+L[1][i]
-    print(dy[1][n-1])
+    print(max(max(dy[0]),max(dy[1])))
