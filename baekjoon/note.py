0c309b37c3297016ed6b9cf09defb6cec42e583a
Jihun Lee <wlgns3398@naver.com>
2022-03-03 13:34:51 +0900

    백준

--- baekjoon/note.py
+++ baekjoon/note.py
@@ -1,22 +1,10 @@
 import sys
 sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
-a = []
-for i in range(9):
-    n =int(input())
-    a.append(n)
-a.sort()
-res=[0]*9
-def DFS(L,s):
-    if L==9:
-        if s==100 and sum(res)==7:
-            for i in range(9):
-                if res[i]==1:
-                    print(a[i])
-            sys.exit(0)    
-        return  
-    else:
-        res[L]= 1
-        DFS(L+1,s+a[L])
-        res[L]=0
-        DFS(L+1,s)
-DFS(0,0)
+
+a, b = map(int ,input().split())
+plus = int(input())
+
+hour = (a+(b+plus)//60)%24
+minute = (b+plus)%60
+print(hour, minute)
+
