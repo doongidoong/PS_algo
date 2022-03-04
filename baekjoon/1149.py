bf6a516dc4b0c2ef903bcd4a715df42ef5f6a5c4
Jihun Lee <wlgns3398@naver.com>
2022-03-04 14:26:16 +0900

    DP 미해결

--- baekjoon/1149.py
+++ baekjoon/1149.py
@@ -1,22 +1,12 @@
 import sys
 sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
 n =int(input())
-a= [ list(map(int,input().split())) for _ in range(n)]
-res = [0]*n
-mind=100000000000
-def DFS(L,s,tot):
-    global mind
-    if tot>mind:
-        return
-    if L==n:
-        if tot<mind:
-            mind = tot
-    else:
-        for i in range(3):
-            if i !=s:
-                res[L] =i
-                DFS(L+1,i,tot+a[L][i])
+a=[ list(map(int,input().split())) for _ in range(n)] 
 
-DFS(0,-1,0)
+for i in range(1,n):
+    a[i][0] += min(a[i-1][1],a[i-1][2]) 
+    a[i][1] += min(a[i-1][0],a[i-1][2]) 
+    a[i][2] += min(a[i-1][0],a[i-1][1])
 
-print(mind)
+print(min(a[n-1])) 
+    
