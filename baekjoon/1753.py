79278e7840689ace32a204e2b9205a5719c89a36
Jihun Lee <wlgns3398@naver.com>
2022-03-08 22:45:18 +0900

    다익스트라

--- baekjoon/1753.py
+++ baekjoon/1753.py
@@ -15,21 +15,25 @@ dis = [inf]*(n+1)
     
 def dijkstra(start):
     q= []
-    dis[start]= 0
-    hq.heappush(q,(0,start))
+    dis[start]= 0 #시작은 0
+    hq.heappush(q,(0,start)) #우선순위큐로 거리로 정렬 
     while q:
-        dist, now = hq.heappop(q)
-        if dist>dis[now]:
+        dist, now = hq.heappop(q) # 가장 가까운 거리순대로 뽑음(처음은 0을 뽑음)
+        if dist>dis[now]: #만약 저장된 거리가 더 짧다면 continue 
             continue
-        for i in g[now]:
-            cost = dist + i[1]
-            if dis[i[0]] >cost:
+        for i in g[now]: # g[now]와 연결된 요소 확인(처음은 start와 연결된 요소들 모두 갱신)
+            cost = dist + i[1] # 지금까지의 거리와 now와 해당 노드의 거리 합치기
+            if dis[i[0]] >cost: # 저장되어 있는 거리와 (지금까지 거리 + now와의 거리) 비교 
                 dis[i[0]] = cost
-                hq.heappush(q,(cost,i[0]))
-
-
-
+                hq.heappush(q,(cost,i[0])) #저장된 거리가 갱신되었다면 우선순위큐도 반영 
 dijkstra(start)
+
+for i in range(1,n+1):
+    if dis[i]==inf:
+        print("INF")
+    else:
+        print(dis[i])
+
 """
 v,e = map(int,input().split())
 start = int(input())
@@ -55,8 +59,3 @@ def dijkstra(start):
 
 dijkstra(start)
 """
-for i in range(1,n+1):
-    if dis[i]==inf:
-        print("INF")
-    else:
-        print(dis[i])
