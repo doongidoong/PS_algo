090aa35dda84e55c8bb11e1b3996f53c145cc91a
Jihun Lee <wlgns3398@naver.com>
2022-03-12 17:11:11 +0900

    스택/큐 복습해결

--- programmers/stackque03.py
+++ programmers/stackque03.py
@@ -1,9 +1,27 @@
 from collections import deque
-def solution(bridge_length, weight, truck_weight):
+def solution(bridge_length, weight, truck_weights):
     answer = 0
-    pos =0
-    bridge = deque()
-    dis= dict()
+    q = deque()
+    for i in range(bridge_length):
+        q.append(0)
+    pos= 0
+    temp= 0
+    out =0
+    while q:
+        answer += 1
+        t= q.popleft()
+        if t==1:
+            temp -=truck_weights[out]
+            out +=1
+        if pos < len(truck_weights):
+            if temp + truck_weights[pos] <= weight:
+                temp+=truck_weights[pos]
+                pos+=1
+                q.append(1)
+            else:
+                q.append(0)
+    
+    return answer
     
 bridge_length=2	
 weight = 10
