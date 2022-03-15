2f2024de50fc26cff6b30986e89c197f602ac344
Jihun Lee <wlgns3398@naver.com>
2022-03-15 21:45:37 +0900

    힙

---
+++ programmers/heap03.py
@@ -0,0 +1,39 @@
+
+import heapq
+
+def solution(operations):
+    minh=[]
+    maxh=[]
+    answer = []
+    cnt=0
+    p = []
+
+    for i in range(len(operations)):
+        order, num = operations[i].split()
+        num = int(num)
+        if order=='I':
+            heapq.heappush(minh,num)
+            heapq.heappush(maxh,num*(-1))
+        else:
+            if num==-1 and minh:
+                heapq.heappop(minh)
+                if minh ==[] or minh[0]>-maxh[0]:
+                    minh=[]
+                    maxh=[]
+            else:
+                heapq.heappop(maxh)
+                if maxh==[] or minh[0]>-maxh[0]:
+                    minh=[]
+                    maxh=[]
+    if minh:
+        b = -heapq.heappop(maxh)
+        a= heapq.heappop(minh)       
+        answer.append(b)
+        answer.append(a)
+    else:
+        answer = [0,0]
+    return answer
+
+operations = ["I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"]
+print(solution(operations))
+
