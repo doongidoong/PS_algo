99389ff610342964175595e2da9783e58b96e8e6
Jihun Lee <wlgns3398@naver.com>
2022-03-17 19:55:23 +0900

    정렬

---
+++ programmers/sort03.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    cnt=0
+    for i in range(len(citations)):
+        if citations[i]<=cnt:
+            break
+        cnt +=1
+    answer = cnt
+    return answer
+
+
+citations=[3, 0, 6, 1, 5]
+
+print(solution(citations))
