d7606d2d4b941cb7a4a13644ce35044790897a4b
Jihun Lee <wlgns3398@naver.com>
2022-03-18 16:48:46 +0900

    완전탐색

---
+++ programmers/bruteforce02.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+s= "17"
+
+prime = [0]*(10000000)
+
+
+def solution(numbers):
+    prime[0]=1
+    prime[1]=1
+
+    for i in range(2,4000):
+        for j in range(2*i,10000000,i):
+            prime[j]=1
+    s = list(numbers)
+    cnt=0
+    for i in range(1,len(s)+1):
+        for j in list(permutations(s, i)):
+            if prime[int(''.join(j))]==0:
+                prime[int(''.join(j))]=1
+                cnt+=1
+    return cnt
+numbers="011"
+print(solution(numbers))
