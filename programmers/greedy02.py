abe0e518b0b5d2beda6fda3551e30ad949ee801f
Jihun Lee <wlgns3398@naver.com>
2022-03-21 00:05:34 +0900

    브루트포스해결

--- programmers/greedy02.py
+++ programmers/greedy02.py
@@ -1,49 +1,24 @@
-# A 65 Z 
-def move(next):
-    return min(ord(next)-ord('A'),ord('Z')-ord(next)+1)
-def findNext(name,now):
-    lt=now
-    rt=now
-    cnt=0
-    while cnt<len(name):
-        cnt+=1
-        lt-=1
-        if lt==-1:
-            lt=len(name)-1
-        rt+=1
-        if rt ==len(name):
-            rt=0
-        if name[lt]!='A' and name[rt]!='A':
-            a,b = findNext(name,lt)
-            c,d = findNext(name,rt)
-            if a>c:
-                return cnt,rt
-            else:
-                return cnt,lt
-        elif name[lt]!='A':
-            return cnt, lt
-        elif name[rt]!='A':
-            return cnt,rt
-    return cnt,now
-
-# AABAAABBBA
 def solution(name):
-    answer = 0
-    now= 0
-    name= list(name)
-    if name[0]!='A':
-        answer +=move(name[0])
-        name[0]='A'
-    while 1:
-        cnt,next = findNext(name,now)
-        print(cnt,now,next)
-        if cnt==len(name):
-            break
-        answer+=cnt
-        answer+=move(name[next])
-        name[next]='A'
-        now=next
+    if set(name) == {'A'}:
+        return 0
+
+    answer = float('inf')
+    for i in range(len(name) // 2): # 반 이상 움직일 필요 없음
+        left_moved = name[-i:]+name[:-i] # 왼쪽으로 봄, i가 증가할 때마다 시작점이 왼쪽으로 한칸
+        right_moved = name[i:]+name[:i] # 오른족으로 봄, i가 증까할때마다 시작점이 오른쪽으로 한칸
+        print(left_moved,right_moved)
+        for n in [left_moved, right_moved[0]+right_moved[:0:-1]]:
+            while n and n[-1] == 'A':
+                n = n[:-1]
+            print(n)
+            row_move = i + len(n)-1
+            col_move = 0
+            for c in map(ord, n):
+                col_move += min(c - 65, 91 - c)
+
+            answer = min(answer, row_move + col_move)
+
     return answer
 
-name = "BBBBBB"	
+name = "JEROEN"
 print(solution(name))
