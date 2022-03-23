f92c2420a86e25016a237584b26b3651e66e9f46
Jihun Lee <wlgns3398@naver.com>
2022-03-23 23:49:18 +0900

    그리디

---
+++ programmers/greedy06.py
@@ -0,0 +1,16 @@
+def solution(routes):
+    routes.sort(key= lambda x: x[1])
+    e=routes[0][1]
+    cnt=1
+    for i in range(1,len(routes)):
+        if routes[i][0]<=e:
+            continue
+        else:
+            e=routes[i][1]
+            cnt+=1
+
+    answer = cnt
+    return answer
+routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
+
+solution(routes)
