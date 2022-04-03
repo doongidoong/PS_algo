0a1095b8734845f1f80755500810cb7e6912cbe4
Jihun Lee <wlgns3398@naver.com>
2022-04-03 23:39:20 +0900

    등굣길_topdown

--- programmers/dp03.py
+++ programmers/dp03.py
@@ -3,18 +3,18 @@ def solution(m, n, puddles):
     dy[0][0]=1
     for i in puddles:
         dy[i[1]-1][i[0]-1]=-1
-    DFS(n-1,m-1,dy)
-    print(dy)
-    return (dy[-1][-1]-2) % 1000000007
-def DFS(y,x,dy):
-    print(y,x)
-    if dy[y][x]==-1 or x<0 or y<0:
-        return 0
-    if dy[y][x]!=0:
-        return dy[y][x]
-    else:
-        dy[y][x] = DFS(y-1,x,dy)+DFS(y,x-1,dy)
-        return dy[y][x]
+    def DFS(y,x):
+        if dy[y][x]==-1 or x<0 or y<0:
+            return 0
+        if dy[y][x]!=0:
+            return dy[y][x]
+        else:
+            dy[y][x] = DFS(y-1,x)+DFS(y,x-1)
+            return dy[y][x]
+    DFS(n-1,m-1)
+    return (dy[-1][-1]) % 1000000007
+
+
 
 
 m= 4	
