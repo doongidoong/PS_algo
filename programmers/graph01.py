76b800bf2eb20a988a488ffa0d19b7a458192ea3
doongidoong <wlgns3398@naver.com>
2022-04-11 23:22:17 +0900

    가장 먼 노드

--- programmers/graph01.py
+++ programmers/graph01.py
@@ -1,25 +1,33 @@
 from cmath import inf
 import heapq
+from collections import deque
 def solution(n, edge):
     answer = 0
+    g=[[] for _ in range(n+1)]
+    for i in edge:
+        g[i[0]].append(i[1])
+        g[i[1]].append(i[0])
+    answer = 0
+    dis =[0 for _ in range(n+1)]
+    dis[1] = 1
+    Q = deque()
+    Q.append((1,1))
+    while Q:
+        p = Q.popleft()
+        for i in g[p[0]]:
+            if dis[i]==0:
+                dis[i]=p[1]+1
+                Q.append((i,dis[i]))
+    answer=0    
+    maxd = max(dis)
+    for i in range(2,n+1):
+        if maxd==dis[i]:
+            answer+=1
+    print(answer)
     return answer
 
-def dijkstra(start):
-    dis = [inf]*(n+1)
-    q=[]
-    heapq.heappush(q,(0,start))
-    dis[start] = 0
-    while q:
-        distance, now  = heapq.heappop(q)
-        if distance > dis[now]:
-            continue
-        for i in g[now]:
-            cost = distance + i[1]
-            if dis[i[0]] > cost:
-                dis[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-    return dis
+
 n = 6
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 
-print(vertex)
+solution(6,vertex)
