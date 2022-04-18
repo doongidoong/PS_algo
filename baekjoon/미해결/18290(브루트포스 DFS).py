ac4af26c83ea36eea9a400bdfc40fd2f579ef901
doongidoong <wlgns3398@naver.com>
2022-04-18 22:38:21 +0900

    NM과 K (1)

---
+++ baekjoon/미해결/18290(브루트포스 DFS).py
@@ -0,0 +1,44 @@
+import sys
+
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+n,m,k = map(int, input().split())
+arr= [list(map(int,input().split()) ) for _ in range(n)]
+check = [[0 for _ in range(m)] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+answer = 0
+def go(px, py, index, sum):
+    if index == k:
+        global answer
+    
+        if answer < sum:
+            answer = sum
+        
+        return
+
+    for x in range(0, n):
+        for y in range(0, m):
+            # 현재 위치 방문했었는지 확인
+            if visited[x][y]:
+                continue
+
+            ok = True
+            # 동서남북 방문 했었는지 확인
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < n and 0 <= ny < m:
+                    if visited[nx][ny]:
+                        ok = False            
+            # 방문
+            if ok:
+                visited[x][y] = True
+                go(x, y, index+1, sum+arr[x][y])
+                visited[x][y] = False
+
+go(0,0,0,0)
+print(answer)
