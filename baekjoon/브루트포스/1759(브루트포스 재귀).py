2d916952b23632973b565b5c7fdd96e79bc7025f
doongidoong <wlgns3398@naver.com>
2022-04-19 11:22:34 +0900

    1, 2, 3, 더하기

---
+++ baekjoon/브루트포스/1759(브루트포스 재귀).py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+n,m=map(int,input().split())
+a = list(sys.stdin.readline().rstrip().split())
+a.sort()
+res=['']*(n)
+check  = ['a','i','e','o','u']
+
+def DFS(L,s,c):
+    if L==n:
+        if c>= 1 and n-c>=2 :
+            for i in res:
+                print(i,end='')
+            print()
+        return
+    else:
+        for i in range(s,m):
+            res[L] = a[i]
+            if a[i] in check:
+                DFS(L+1,i+1,c+1)
+            else:
+                DFS(L+1,i+1,c)
+
+DFS(0,0,0)
