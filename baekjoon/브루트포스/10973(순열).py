840698c3fb0856a0f1e01485bc3f4f82868ff4bd
doongidoong <wlgns3398@naver.com>
2022-04-23 19:01:21 +0900

    이전순열

---
+++ baekjoon/브루트포스/10973(순열).py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+
+input = sys.stdin.readline
+#from itertools import permutations
+n = int(input())
+
