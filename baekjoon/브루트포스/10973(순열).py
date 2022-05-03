200866d7782140a6022331adb40acaabcbf4c563
doongidoong <wlgns3398@naver.com>
2022-05-03 11:36:58 +0900

    이전순열

--- baekjoon/브루트포스/10973(순열).py
+++ baekjoon/브루트포스/10973(순열).py
@@ -5,3 +5,16 @@ input = sys.stdin.readline
 #from itertools import permutations
 n = int(input())
 
+L= list(map(int,input().split()))
+
+
+for i in range(n-1,0,-1):
+    if L[i-1]>L[i]:
+        for j in range(n-1,0,-1):
+            if L[i-1]>L[j]:
+                L[i-1], L[j] = L[j],L[i-1]
+                ans = L[:i]+ sorted(L[i:], reverse=True)
+                print(*ans)
+                sys.exit(0)
+print(-1)
+
