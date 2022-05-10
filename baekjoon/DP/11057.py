2e3ae80cf377a7677fc0e078f6342305c0d51545
doongidoong <wlgns3398@naver.com>
2022-05-10 22:41:11 +0900

    오르막 수

--- baekjoon/DP/11057.py
+++ baekjoon/DP/11057.py
@@ -1,17 +1,29 @@
 import sys
-sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 
 
 n = int(input())
 dy = [[0]*10 for _ in range(n+1)]
-for i in range(0,10):
-    dy[1][i] = 1
 
+for i in range(10):
+    dy[1][i]=1
 
 for i in range(2,n+1):
-    for j in range(10):
-        if j ==0 :
-            dy[i][j] =sum(dy[i-1])
-        else:
-            dy[i][j] = dy[i][j-1] - dy[i-1][j-1]
-print(sum(dy[n]))
+    dy[i][0] = dy[i-1][0]
+    for j in range(1,10):
+        dy[i][j] = dy[i][j-1]+dy[i-1][j]
+
+print(sum(dy[n])%10007)
+
+
+# for i in range(0,10):
+#     dy[1][i] = 1
+
+
+# for i in range(2,n+1):
+#     for j in range(10):
+#         if j ==0 :
+#             dy[i][j] =sum(dy[i-1])
+#         else:
+#             dy[i][j] = dy[i][j-1] - dy[i-1][j-1]
+# print(sum(dy[n]))
