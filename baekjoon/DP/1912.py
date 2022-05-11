ae9dfcb6cb850c6a48d18722d6944c4f0866429e
doongidoong <wlgns3398@naver.com>
2022-05-11 21:33:33 +0900

    연속합

--- baekjoon/DP/1912.py
+++ baekjoon/DP/1912.py
@@ -1,10 +1,14 @@
 import sys
-sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+
 n = int(input())
 L = list(map(int,input().split()))
-dy = [0 for _ in range(n+1)]
-L.insert(0,0)
-for i in range(1,n+1):
-    dy[i]= max(dy[i-1],0)+L[i]
 
-print(max(dy[1:]))
+dp = [0 for i in range(n)]
+
+dp[0] = L[0]
+for i in range(1,n):
+    dp[i] = max(dp[i-1],0)+L[i]
+
+print(max(dp))
