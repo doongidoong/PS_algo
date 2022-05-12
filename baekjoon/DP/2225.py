323ca81683c63f82331fbe8ce403899bd2ff99be
doongidoong <wlgns3398@naver.com>
2022-05-12 13:16:24 +0900

    포도주 시식

--- baekjoon/DP/2225.py
+++ baekjoon/DP/2225.py
@@ -1,17 +1,14 @@
-
 import sys
-sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 n,k = map(int,input().split())
-s=201
+s=7
 dy = [[0]*s for j in range(s)]
 
 for i in range(s):
-    dy[1][i] = i
-    dy[i][1] = i
-
-
-for i in range(2,s):
+    dy[1][i] = 1
+    dy[2][i] = i+1
+    
+for i in range(3,s):
     for j in range(s):
-        dy[j][i]  = dy[j-1][i] + dy[j][i-1]
-
+        dy[i][j] = dy[i-1][j]+ dy[i][j-1]
 print(dy[k][n]%1000000000)
