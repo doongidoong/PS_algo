b0caf1a7233319c5219a09ddf0a26b7690b79e42
doongidoong <wlgns3398@naver.com>
2022-05-14 23:01:56 +0900

    타일 채우기

--- baekjoon/DP/2133.py
+++ baekjoon/DP/2133.py
@@ -1,11 +1,12 @@
-N = int(input())
+import sys
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 
-d = [0]*31
-d[0] = 1
-
-for i in range(2, N+1, 2):
-    d[i] = d[i-2] * 3
-    for j in range(0, i-2, 2):
-        d[i] += d[j] * 2
-
-print(d[N])
+n = int(input())
+dp = [0 for i in range(31)]
+dp[2] = 3
+for i in range(4, 31, 2):
+    dp[i] = dp[2] * dp[i - 2]
+    for j in range(4, i, 2):
+        dp[i] += 2 * dp[i - j]
+    dp[i] += 2
+print(dp[n])
