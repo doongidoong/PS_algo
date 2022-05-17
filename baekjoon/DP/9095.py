f75203d7eb40b446947fbca6fbec84afaad8cdf8
doongidoong <wlgns3398@naver.com>
2022-05-17 14:17:44 +0900

    1, 2, 3 더하기

--- baekjoon/DP/9095.py
+++ baekjoon/DP/9095.py
@@ -1,5 +1,6 @@
+
 import sys
-sys.stdin = open("C:\\Users\\82103\\pythonprogramming\\baekjoon\\input.txt","rt")
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 n = int(input())
 dy =[0,1,2,4]
 for _ in range(n):
