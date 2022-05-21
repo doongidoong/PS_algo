c39277057084adb98e0880ad3ee433305a7e3c03
doongidoong <wlgns3398@naver.com>
2022-05-21 22:51:12 +0900

    숨박꼭질 3

---
+++ baekjoon/그래프/다익스트라/13549.py
@@ -0,0 +1,22 @@
+from cmath import inf
+import sys
+import heapq 
+
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+n,k = map(int, input().split())
+Q = [] 
+heapq.heappush(Q,(0,n))
+dist = [inf]*200001
+while Q:
+    now = heapq.heappop(Q)
+    if now[1]==k:
+        print(now[0])
+        break    
+    if 2*now[1] <= 2*k and now[0]<dist[2*now[1]]:
+        dist[2*now[1]] =now[0]
+        heapq.heappush(Q,(now[0],2*now[1]))
+    for i in (now[1]+1, now[1]-1):
+        if 0<=i<=200001 and now[0]+1<dist[i]:
+            dist[i] =now[0]+1
+            heapq.heappush(Q,(now[0]+1,i))
+
