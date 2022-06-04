ac5ba9249e113bf8d5752fba07fd478abe89fc61
doongidoong <wlgns3398@naver.com>
2022-06-04 23:48:21 +0900

    LCS

--- baekjoon/DP/LIS/9251(미해결).py
+++ baekjoon/DP/LIS/9251(미해결).py
@@ -2,13 +2,13 @@ import sys
 sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 m = list(input())
 n = list(input())
-m_len = len(m)
-n_len = len(n)
-dp = [[0] * (n_len + 1) for i in range(m_len + 1)]
-for i in range(m_len):
-    for j in range(n_len):
+l1 = len(m)
+l2 = len(n)
+dp = [[0] * (l2 + 1) for i in range(l1 + 1)]
+for i in range(l1):
+    for j in range(l2):
         if m[i] == n[j]:
             dp[i + 1][j + 1] = dp[i][j] + 1
         else:
             dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
-print(dp[m_len][n_len])
+print(dp[l1][l2])
