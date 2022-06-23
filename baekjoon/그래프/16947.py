8ba38f3d720a94e1a6b0c3c1079e6db5c4367696
doongidoong <wlgns3398@naver.com>
2022-06-23 23:55:43 +0900

    서울지하철 2호선

--- baekjoon/그래프/16947.py
+++ baekjoon/그래프/16947.py
@@ -1,33 +1,52 @@
 import sys
-sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
-    
-n = int(input())
+from collections import deque
+sys.setrecursionlimit(10**9)
 
-g= [[0]*(n+1) for _ in range(n+1)]
+# DFS를 통해 사이클 찾기
 
-for i in range(n):
-    a,b =map(int,input().split())
-    g[a][b] = 1
-    g[b][a] = 1
+def dfs(x, cnt):
+    # 이미 방문한 노드인데 거리차가 3 이상일 경우 사이클
+    if check[x]:
+        if cnt - dist[x] >= 3:
+            return x
+        else: return -1
+    check[x] = 1
+    dist[x] = cnt
+    for y in adj_list[x]:
+        cycleStartNode = dfs(y, cnt + 1)
+        if cycleStartNode != -1:
+            check[x] = 2
+            if x == cycleStartNode: return -1
+            else: return cycleStartNode
+    return -1
 
-print(g)
-p =[-1]*(n+1)
-def find(a):
-    while p[a]>0:
-        a= p[a]
-    return a
+if __name__ == '__main__':
+    N = int(input())
+    adj_list = [[] * (N + 1) for _ in range(N + 1)]
+    # check[i] = 0 : 방문하지 않은 노드
+    # check[i] = 1 : 방문한 노드
+    # check[i] = 2 : 사이클에 속하는 노드
+    check = [0] * (N + 1)
+    dist = [0] * (N + 1)
 
-def union(a,b):
-    ta = find(a)
-    tb = find(b)
-    if ta>tb:
-        p[tb] += p[ta]
-        
-        p[ta] = tb
-    else:
-        print(ta)
-        p[ta] += p[tb]
-        p[tb] = ta
-
-union(1,2)
-print(p)
+    for _ in range(N):
+        u, v = map(int, sys.stdin.readline().split())
+        adj_list[u].append(v)
+        adj_list[v].append(u)
+    # 사이클 찾기
+    dfs(1, 0)
+    # BFS를 통해 사이클까지의 거리 계산하기
+    q = deque()
+    for i in range(1, N + 1):
+        if check[i] == 2:
+            q.append(i)
+            dist[i] = 0
+        else:
+            dist[i] = -1
+    while q:
+        x = q.popleft()
+        for y in adj_list[x]:
+            if dist[y] == -1:
+                q.append(y)
+                dist[y] = dist[x] + 1
+    print(' '.join(map(str, dist[1:])))
