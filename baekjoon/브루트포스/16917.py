7a14081904e6555cdc1ef25e244002211d18dd09
doongidoong <wlgns3398@naver.com>
2022-06-28 00:07:08 +0900

    양념반후라이드반

--- baekjoon/브루트포스/16917.py
+++ baekjoon/브루트포스/16917.py
@@ -2,4 +2,20 @@ import sys
 sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
 
 a,b,c,x,y = map(int,input().split())
-
+ans = 0
+if a+b > 2 * c:
+    if a>2*c and b>2*c:
+        temp = max(x,y)
+    elif b > 2 * c:
+        temp = y
+    elif a> 2*c :
+        temp = x
+    else:
+        temp = min(x,y)
+    ans = temp*c*2
+    x = max(x-temp,0)
+    y = max(y-temp,0)
+    ans += x*a + y*b
+else:
+    ans += x*a + y*b
+print(ans)
