5905f23c0e05e822e7ae4f701c15e8d31a80eaf6
doongidoong <wlgns3398@naver.com>
2022-07-04 23:48:36 +0900

    점프점프

--- baekjoon/DP/11060.py
+++ baekjoon/DP/11060.py
@@ -1,3 +1,22 @@
 
+from cmath import inf
 import sys
-sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\pythonprogramming\\baekjoon\\input.txt","rt")
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\baekjoon\\input.txt","rt")
+
+n= int(input())
+
+L = list(map(int,input().split()))
+
+dy=[inf for _ in range(n)]
+
+dy[0]=0
+for i in range(n):
+    temp = dy[i]
+    ran = min(L[i]+1,n-i)
+    for j in range(1,ran):
+        dy[i+j] = min(dy[i+j],temp+1)
+
+if dy[-1]==inf:
+    print(-1)
+else :
+    print(dy[-1])
