640a5d81cc8a3c4829a9f923982800ebbb7505b2
doongidoong <wlgns3398@naver.com>
2022-07-08 00:11:57 +0900

    로마 숫자 만들기

--- baekjoon/브루트포스/16922.py
+++ baekjoon/브루트포스/16922.py
@@ -1,12 +1,20 @@
-from itertools import combinations_with_replacement
-n = int(input())
-result = []
+#순열이 아닌 조합으로 풀어야 한다.
+import sys
+sys.stdin = open("C:\\Users\\wlgns\\pythonprogramming\\baekjoon\\input.txt","rt")
+n=int(sys.stdin.readline())
+
 numbers = [1, 5, 10, 50]
+res = [0 for _ in range(100001)]
+count =0
+def DFS(L,t,s):
+    global count 
+    if L==n:
+        if res[t]==0:
+            count+=1
+            res[t]=1
+        return
+    for i in range(s,4):
+        DFS(L+1,t+numbers[i],i)
 
-for temp in combinations_with_replacement(range(4), n):
-    sum = 0
-    for i in temp:
-        sum += numbers[i]
-    result.append(sum)
-
-print(len(set(result)))
+DFS(0,0,0)
+print(count)
