8a9f0aa91c877286731ab3054a7859aafe40ed10
doongidoong <wlgns3398@naver.com>
2022-07-08 23:50:07 +0900

    신고 결과 받기

--- programmers/kakao2022_1.py
+++ programmers/kakao2022_1.py
@@ -3,18 +3,21 @@ id_list = ["muzi", "frodo", "apeach", "neo"]
 report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 k=2
 
-d = defaultdict(set)
-
 def solution(id_list, report, k):
-    for i in range(len(report)):
-        a, b =report[i].split()
-        d[b].add(a)
-    for i in range(len(id_list)):
-        if len(d[id_list[i]]) >=k:
-            for j in d[id_list[i]]:
-                print(j)
-
-    answer = []
+    d = defaultdict(set)
+    index = {}
+    id_len = len(id_list)
+    answer = [0 for _ in range(id_len)]
+    reports = [[0 for _ in range(id_len)] for _ in range(id_len)]
+    for i in range(id_len):
+        index[id_list[i]] = i
+    for i in report:
+        a,b = i.split()
+        reports[index[b]][index[a]] =1
+    for i in range(id_len):
+        if sum(reports[i])>=k:
+            for j in range(id_len):
+                if reports[i][j]:
+                    answer[j]+=1
     return answer
-
-solution(id_list,report,k)
+print(solution(id_list,report,k))
