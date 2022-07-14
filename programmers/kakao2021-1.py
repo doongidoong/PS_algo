3676d8be7c2d8ff055785c2d90e85a0647b89395
doongidoong <wlgns3398@naver.com>
2022-07-14 23:08:23 +0900

    숫자 문자열과 영단어

---
+++ programmers/kakao2021-1.py
@@ -0,0 +1,18 @@
+hash = {"zero":0, "one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8 ,"nine":9 }
+
+def solution(s):
+    temp=""
+    answer=""
+    for i in s:
+        if 97<=ord(i) and ord(i)<=122:
+            temp+=i
+            if hash.get(temp,-1)!=-1:
+                answer= answer+ str(hash[temp])
+                temp=""
+        else:
+            answer+=i
+
+        
+    return int(answer)
+
+print(solution("1zerotwozero3"))
