9a1128adba2b33d3249df9b03cde0985b1d57902
doongidoong <wlgns3398@naver.com>
2022-07-22 23:52:54 +0900

    같은 숫자는 싫어

--- programmers/stack.py
+++ programmers/stack.py
@@ -1,5 +1,7 @@
 def solution(arr):
     answer = []
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    for i in arr:
+        if len(answer)==0 or i != answer[-1]:
+            answer.append(i)
+    
     return answer
