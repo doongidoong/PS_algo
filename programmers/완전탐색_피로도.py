8249d5550b0837ef2b2a18b00c285fbe29277bf8
doongidoong <wlgns3398@naver.com>
2022-07-27 23:56:18 +0900

    음양더하기

--- programmers/완전탐색_피로도.py
+++ programmers/완전탐색_피로도.py
@@ -1,22 +1,6 @@
-check = []
-answer =0
 def solution(k, dungeons):
-    global check, answer
-    check = [0]*len(dungeons)
-    DFS(0,dungeons,k)
+    answer = -1
     return answer
-
-
-res = [0,0,0]
-def DFS(level,dungeons,point):
-    global answer
-    answer = max(answer,level)
-    
-    for i in range(len(dungeons)):
-        if check[i]==0 and point-dungeons[i][0]>=0:
-            check[i]= 1
-            DFS(level+1,dungeons,point-dungeons[i][1])
-            check[i]=0
 
 k= 80
 
