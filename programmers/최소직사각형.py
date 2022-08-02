e4dfa856d38549a9ed12f5179af726db7f002434
doongidoong <wlgns3398@naver.com>
2022-08-02 23:58:43 +0900

    최소직사각형

--- programmers/최소직사각형.py
+++ programmers/최소직사각형.py
@@ -1,7 +1,12 @@
 def solution(sizes):
-    answer = 0
+    minN=maxN=0
+    for i in sizes:
+        minN = max(minN,min(i[0], i[1]))
+        maxN = max(maxN, max(i[0],i[1]))
+    answer = maxN*minN
     return answer
 
 
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+sizes =[[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
+print(solution(sizes))
