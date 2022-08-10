724e316d7c890d08d9e87b48ef2c57a15a3b0eae
doongidoong <wlgns3398@naver.com>
2022-08-10 22:50:09 +0900

    모음사전

--- programmers/모음사전.py
+++ programmers/모음사전.py
@@ -1,13 +1,3 @@
-d = { 'A':1, 'E':2, 'I':3, 'O':4, 'U':5}
-    
 def solution(word):
-
-    print(word)
     answer = 0
     return answer
-
-2*(781)+3*()+4*()+3
-'EIO'
-def DFS(L, num):
-    global word
-    num += d([word[L]]-1)*(3905/(5^L))+1
