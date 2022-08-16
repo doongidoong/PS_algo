1621dee8fed91aa491e09015479a99fb1f0f4901
doongidoong <wlgns3398@naver.com>
2022-08-16 23:56:12 +0900

    조이스틱

--- programmers/조이스틱.py
+++ programmers/조이스틱.py
@@ -6,9 +6,7 @@ def solution(name):
     
     min_move = len(name) - 1
     for i, c in enumerate(name):
-        
         next_i = i+1
-        
         while next_i < len(name) and name[next_i] == 'A':
             next_i += 1
         # 각 문자부터 'A..'문자가 있을경우 몇번씩 조이스틱쓰는지 체크
