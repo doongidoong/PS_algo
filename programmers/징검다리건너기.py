95ed98eab9b754718ca70d19ce74acad93345d58
doongidoong <wlgns3398@naver.com>
2022-09-18 17:40:51 +0900

    징검다리건너기

--- programmers/징검다리건너기.py
+++ programmers/징검다리건너기.py
@@ -1,3 +1,20 @@
 def solution(stones, k):
-    answer = 0
+    answer = 200000001
+    lt = min(stones)
+    rt = max(stones)
+    while lt<=rt:
+        mid = (lt+rt)//2
+        people = 0
+        for i in stones :
+            cnt = i - mid
+            if cnt <= 0:
+                people += 1
+            else:
+                people = 0
+            if people >= k:
+                answer = min(answer,mid)
+                rt = mid-1
+                break
+        else:
+            lt = mid+1
     return answer
